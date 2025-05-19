1db48b76bfd34e1964800d55448c44d7be424545
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-05-19 14:33:45 +0200

    fix: use queries complete autocomplete in get

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,16 +1,19 @@
 const db = require('../db/queries');
 
-function indexGet(req, res) {
+exports.indexGet = async (req, res) => {
   res.render('index');
-}
+};
 
-function createBookGet(req, res) {
-  console.log(db.getAllAuthors());
+exports.createBookGet = async (req, res) => {
+  const authors = await db.getAllAuthors();
+  const authorNames = authors.map((author) => author.name);
+  console.log(authorNames);
+
+  const genres = await db.getAllGenres();
+  const genreNames = genres.map((genre) => genre.name);
   res.render('createBook', {
     title: 'Add Book',
-    authors: ['Bob', 'Dylan'],
-    genres: ['sci-fi', 'bio'],
+    authors: authorNames,
+    genres: genreNames,
   });
-}
-
-module.exports = { indexGet, createBookGet };
+};
