882a8f7a1e5be03345afa2d333190a2dbce63d7a
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-05-19 16:25:30 +0200

    chore: add a cleardb script

---
+++ db/cleardb.js
@@ -0,0 +1,27 @@
+const { Client } = require('pg');
+require('dotenv').config();
+
+const SQL = `
+DELETE FROM books;
+
+DELETE FROM authors;
+
+DELETE FROM genres;
+
+DELETE FROM books_authors;
+
+DELETE FROM books_genres;
+`;
+
+async function main() {
+  console.log('clearing...');
+  const client = new Client({
+    connectionString: `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log('done');
+}
+
+main();
